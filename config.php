d25df81ffbb2eb3327c97aeb37ce1b3509b8a094
Fachri Juliandra <fachriyusuf037@gmail.com>
2024-05-11 21:57:03 +0700

    first commit

---
+++ config.php
@@ -0,0 +1,6 @@
+<?php
+$host = 'localhost';
+$userdb = 'root';
+$passdb = '';
+$db = 'psasfachri';
+$con = mysqli_connect($host, $userdb, $passdb, $db);
---
+++ home.php
@@ -0,0 +1,20 @@
+<div class="card">
+    <div class="card-body">
+        <p>
+            <img src="gambar/well.jpg" class="rounded float-end" alt="WebDev" height="250px">
+            <b><i> Navy SEALs</i></b> merupakan salah satu unit pasukan elit atau pasukan komando terkenal asal
+            Amerika Serikat
+            (AS). Pasukan yang terkenal dengan lencana (insignia) "SEAL Trident" berupa burung elang yang
+            mencengkeram jangkar AL AS, tombak trisula (trident) dan sebuah pistol kuno, dikenal sebagai salah
+            satu pasukan elit terbaik dan tertangguh di dunia militer saat ini.
+            <br>
+            Dilansir Britannica, <b><i>Navy SEALs</i></b> merupakan unit pasukan AL AS yang dibentuk pada tahun
+            1962 serta
+            dilatih secara intensif untuk memiliki kemampuan melakukan serangan langsung terhadap target musuh
+            di manapun berada, melakukan misi pengintaian tempur serta mengambil bagian dalam aksi melawan
+            kelompok teroris. Meskipun berasal dari kecabangan AL AS, namun sesuai namanya (SEAL/Sea, Air,
+            Land), Navy SEALs mampu beroperasi dari dan di segala medan baik di lautan, daratan hingga terjun
+            payung dari udara.
+        </p>
+    </div>
+</div>
